02d06ddcabf35dbd74e868806e0c3d0fc532274b
Gustavo Hernandez <gnetboy.y@gmail.com>
2020-06-21 04:50:45 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,9 @@
 def my_collect(array)
-  
+ name.split(" ").first 
 i=0
 collection =[]
 while i < array.length
 collection = yield(array[i]) 
-item.split(" ").first
-item.upcase
 i +=1
 end
 collection
